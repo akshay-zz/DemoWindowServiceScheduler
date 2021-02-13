b3f5de24393461718eb464719a1f07335cf3b469
aksha <aksha@WTH-DEV-AKS>
2021-02-13 17:13:59 +0530

    Add window service

---
+++ DemoWindowServiceScheduler/DemoService.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoWindowServiceScheduler
+{
+    public class DemoService
+    {
+        // function name is not mandatory to be as Start
+        public void Start()
+        {
+            // Your logic
+        }
+
+        public void Stop()
+        {
+            // Your logic
+        }
+    }
+}
---
+++ DemoWindowServiceScheduler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Topshelf;
+
+namespace DemoWindowServiceScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var hostFactory = HostFactory.Run(hostConfigurator =>
+            {
+                hostConfigurator.SetServiceName("DemoWindowService");
+                hostConfigurator.SetDisplayName("DemoWindowService");
+                hostConfigurator.SetDescription("Service for testing.");
+
+                hostConfigurator.RunAsLocalSystem();
+
+                hostConfigurator.Service<DemoService>(serviceConfigurator =>
+                {
+                    serviceConfigurator.ConstructUsing(() => new DemoService());
+
+                    serviceConfigurator.WhenStarted(service => service.Start());
+                    serviceConfigurator.WhenStopped(service => service.Stop());
+                });
+            });
+
+            Environment.ExitCode = (int)Convert.ChangeType(hostFactory, hostFactory.GetTypeCode());
+        }
+    }
+}
